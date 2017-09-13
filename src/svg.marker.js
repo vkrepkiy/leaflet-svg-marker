556767c11a214b0bd6e51333764183706eee2e56
Valentin Krepkiy <valkrepkiy@gmail.com>
2017-09-14 00:38:05 +0300

    Fix positioning;

--- src/svg.marker.js
+++ src/svg.marker.js
@@ -17,19 +17,21 @@ export default L.SvgPathMarker = L.Path.extend({
 	initialize: function (latlng, options) {
 		L.Util.setOptions(this, options);
 
-		this._direction   = this.options.direction;
-		this._fillColor   = this.options.fillColor;
-		this._fillOpacity = this.options.fillOpacity;
-		this._latlng      = L.latLng(latlng);
-		this._lineCap     = this.options.lineCap;
-		this._lineJoin    = this.options.lineJoin;
-		this._pathString  = this.options.pathString;
-		this._size        = L.point(this.options.size);
-		this._strokeColor = this.options.strokeColor;
-		this._scale       = this.options.scale;
-		this._scaleBase   = this.options.scaleBase;
-		this._scalePow    = this.options.scalePow;
-		this._weight      = this.options.weight;
+		this._direction    = this.options.direction;
+		this._fillColor    = this.options.fillColor;
+		this._fillOpacity  = this.options.fillOpacity;
+		this._latlng       = L.latLng(latlng);
+		this._lineCap      = this.options.lineCap;
+		this._lineJoin     = this.options.lineJoin;
+		this._originalSize = L.point(this.options.size);
+		this._pathString   = this.options.pathString;
+		this._size         = L.point(this.options.size);
+		this._strokeColor  = this.options.strokeColor;
+		this._scale        = this.options.scale;
+		this._scaleBase    = this.options.scaleBase;
+		this._scalePow     = this.options.scalePow;
+		this._scaleVal     = 1;
+		this._weight       = this.options.weight;
 	},
 	_containsPoint: function (p) {
 		return p.distanceTo(this._point) <= this._size.x + this._clickTolerance();
@@ -42,7 +44,16 @@ export default L.SvgPathMarker = L.Path.extend({
 	},
 	_project: function () {
 		this._point = this._map.latLngToLayerPoint(this._latlng);
+		this._scaleSize();
 		this._updateBounds();
+	},
+	_scaleSize: function () {
+		if (this._scale) {
+			let currentZoom = this._map.getZoom();
+
+			this._scaleVal = Math.pow(currentZoom / this._scaleBase, this._scalePow);
+			this._size     = L.point(this._originalSize).multiplyBy(this._scaleVal);
+		}
 	},
 	setLatLng: function (latlng) {
 		this._latlng = L.latLng(latlng);
@@ -55,38 +66,33 @@ export default L.SvgPathMarker = L.Path.extend({
 		}
 	},
 	_updateBounds: function () {
-    var { x, y } = this._size;
-    var w = this._clickTolerance()
-		var p = [x + w, y + w];
+    var { x, y }  = this._size;
+    var tolerance = Math.floor(this._clickTolerance() * this._scaleVal);
+		var point = [x / 2 + tolerance, y / 2 + tolerance];
 
-		this._pxBounds = new L.Bounds(this._point.subtract(p), this._point.add(p));
+		this._pxBounds = new L.Bounds(this._point.subtract(point), this._point.add(point));
 	},
 	_updatePath: function () {
-		let coeff = 1;
-
-		if (this._scale) {
-			let currentZoom = this._map.getZoom();
-			coeff = Math.pow(currentZoom / this._scaleBase, this._scalePow);
-		}
-
-		this._renderer._updateSvgMarker(this, coeff);
+		this._renderer._updateSvgMarker(this);
 	}
 });
 
 L.Canvas.include({
-  _updateSvgMarker: function (layer, coeff) {
+  _updateSvgMarker: function (layer) {
 		if (!this._drawing || layer._empty()) {
 			return;
 		}
 
     var ctx     = this._ctx;
     var point   = layer._point;
-    var center  = layer._size.divideBy(2).round();
+		var center  = layer._size.divideBy(2);
+
 		var path = svgpath(layer._pathString)
-			.scale(coeff)
-      .rotate(layer._direction, center.x, center.y)
-			.translate(point.x, point.y)
-      .toString();
+			.scale(layer._scaleVal)
+			.rotate(layer._direction, center.x, center.y)
+			.translate(point.x - center.x, point.y - center.y)
+			.toString();
+
     var icon = new Path2D(path);
 
     this._drawnLayers[layer._leaflet_id] = layer;
